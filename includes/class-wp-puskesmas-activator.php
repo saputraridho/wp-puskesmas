0bfcbcc341e7a193a0136421902e1231f631ecae
saputra <saputraridh@gmail.com>
2023-01-11 15:32:46 +0700

    1.0

---
+++ includes/class-wp-puskesmas-activator.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Fired during plugin activation
+ *
+ * @link       https://github.com/saputraridho/
+ * @since      1.0.0
+ *
+ * @package    Wp_Puskesmas
+ * @subpackage Wp_Puskesmas/includes
+ */
+
+/**
+ * Fired during plugin activation.
+ *
+ * This class defines all code necessary to run during the plugin's activation.
+ *
+ * @since      1.0.0
+ * @package    Wp_Puskesmas
+ * @subpackage Wp_Puskesmas/includes
+ * @author     saputra <saputraridh@gmail.com>
+ */
+class Wp_Puskesmas_Activator {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function activate() {
+
+	}
+
+}
---
+++ includes/class-wp-puskesmas-deactivator.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Fired during plugin deactivation
+ *
+ * @link       https://github.com/saputraridho/
+ * @since      1.0.0
+ *
+ * @package    Wp_Puskesmas
+ * @subpackage Wp_Puskesmas/includes
+ */
+
+/**
+ * Fired during plugin deactivation.
+ *
+ * This class defines all code necessary to run during the plugin's deactivation.
+ *
+ * @since      1.0.0
+ * @package    Wp_Puskesmas
+ * @subpackage Wp_Puskesmas/includes
+ * @author     saputra <saputraridh@gmail.com>
+ */
+class Wp_Puskesmas_Deactivator {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function deactivate() {
+
+	}
+
+}
